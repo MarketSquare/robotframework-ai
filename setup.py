98b25616bfd2235259f42cd514921f2572cfafef
Markus <5188411+Noordsestern@users.noreply.github.com>
2024-09-12 17:41:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,11 @@ Programming Language :: Python :: 3.11
 Programming Language :: Python :: 3.12
 """.strip().splitlines()
 
+
+def get_long_description():
+    with open("README.md", "r") as file:
+        return file.read()
+
 def get_requirements():
     with open("requirements.txt", "r") as file:
         return file.readlines()
@@ -24,6 +29,8 @@ setup(
     author_email="stijndejong125@gmail.com",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
     include_package_data=True,
     package_data={"": ["*.misc"]},
     python_requires=">=3.8",
